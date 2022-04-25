7f58b7f114c7c75c2b51a009a77b0de2f633c648
Mike Sanders <msanders331@gmail.com>
2022-04-24 21:12:33 -0700

    added server file and created the seed and date files

---
+++ server.js
@@ -0,0 +1,17 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./Routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () =>{
+        console.log('Server Running')
+    })
+})
