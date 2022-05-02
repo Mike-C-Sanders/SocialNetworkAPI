b3f67f1405544914393366bf2386bdc0eafc2efe
Mike Sanders <msanders331@gmail.com>
2022-05-01 23:52:37 -0700

    fixed an error in the delete reaction function

--- Controllers/reactionController.js
+++ Controllers/reactionController.js
@@ -4,7 +4,7 @@ const {Thought} = require('../Models')
 module.exports = {
     // POST to create a reaction stored in a single thought's reactions array field
     createReaction(req, res) {
-        Thought.findOne(
+        Thought.findOneAndUpdate(
             {_id: req.params.thoughtId},
             {addToSet: {reactions: req.body}},
             {new: true}
@@ -14,6 +14,15 @@ module.exports = {
                 ? res.status(400).json({message: 'Creation of a reaction failed. Check your params or body'})
                 : res.status(200).json(reaction)
         })
-    }
+    },
     // DELETE to pull and remove a reaction by the reaction's reactionId value
+    deleteReaction(req, res){
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+
+
+        ).then((reaction)=>{
+
+        })
+    }
 }
