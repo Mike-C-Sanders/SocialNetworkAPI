8dd7d9d2a7008640f300bc55e5a153a135dc709f
Mike Sanders <msanders331@gmail.com>
2022-05-02 21:23:35 -0700

    created the thought Routes js folder and updated all routes with controller functions.

--- Routes/Api/thoughtRoutes.js
+++ Routes/Api/thoughtRoutes.js
@@ -1,12 +1,28 @@
 // /api/thoughts
 const router = require('express').Router();
 
+//all thought controller functions
+const {
+    getThoughts,
+    getSingleThought,
+    createNewThought,
+    updateThought,
+    deleteThought,
+} = require('../../Controllers/thoughtController');
+
 //GET All Thoughts
+//Post Create a new Thought - push to user thought array
+router.route('/thoughts')
+    .get(getThoughts)
+    .post(createNewThought);
 
 //Get Single THought
-
-//Post Create a new Thought - push to user thought array
-
 //Put update a Thought by _id
+//Delete a thought by id
+router.route('/thoughts/:thoughtId')
+    .get(getSingleThought)
+    
+    .put(updateThought)
+    .delete(deleteThought);
 
-//Delete delete
+module.exports = router;
